067f5fb59c5fe68382ac3c2d1fb1af10a6588cdf
Kenneth Alvarez <kenbalv@gmail.com>
2020-07-28 14:16:24 -0700

    Implemented Edit Leave Type

--- leave-management/Controllers/LeaveTypesController.cs
+++ leave-management/Controllers/LeaveTypesController.cs
@@ -56,6 +56,7 @@ namespace leave_management.Controllers
                     return View(model);
                 }
 
+                //Map the model after LeaveType and save to a variable
                 var leaveType = _mapper.Map<LeaveType>(model);
 
                 //Assign value to Date Created
@@ -73,28 +74,53 @@ namespace leave_management.Controllers
             }
             catch
             {
-                return View();
+                ModelState.AddModelError("", "Something Went Wrong...");
+                return View(model);
             }
         }
 
         // GET: LeaveTypesController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var leaveType = _repo.FindById(id);
+            var model = _mapper.Map<LeaveType, LeaveTypeVM> (leaveType);
+            
+            return View(model);
         }
 
         // POST: LeaveTypesController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, LeaveTypeVM model)
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                //Map the model after LeaveType and save to a variable
+                var leaveType = _mapper.Map<LeaveType>(model);
+
+                //Model doesn't retain info passed into view when submitting
+                //leaveType.DateCreated = DateTime.Now;
+
+                //Call the repository operation to update
+                var isSuccess = _repo.Update(leaveType);
+
+                if (!isSuccess)
+                {
+                    ModelState.AddModelError("", "Something went wrong...");
+                    return View(model);
+                }
+
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                ModelState.AddModelError("", "Something went wrong...");
+                return View(model);
             }
         }
 
